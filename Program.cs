968301c104ce8008cf98a1301e42d1ca9dfe3da4
guillzera <nascimento.farias@pucpr.edu.br>
2025-04-10 20:02:08 -0300

    metodo somar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double numero1 = 10;
+double numero2 = 20;
+double resultado = Somar(numero1, numero2);
+Console.WriteLine($"A soma de {numero1} e {numero2} é: {resultado}");
+Console.ReadLine();
+
+static double Somar(double a, double b)
+{
+    return a + b;
+}
